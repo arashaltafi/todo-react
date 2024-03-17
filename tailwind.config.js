4f91f5ad42165b4ef88a6a46cb96b2f6d5ecb27c
Arash Altafi <arashaltafi1377@gmail.com>
2024-03-17 15:18:00 +0330

    add packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#ffffff",
+        icewhite: "#ebebeb",
+        grey: "#5d5d5d",
+        lightblue: "#9daaf2",
+        darkblue: "#1a2238",
+        blackblue: "#0c1220",
+        orange: "#ff6a3d",
+        tastycolor: "#70B9BE",
+        silentmooncolor: "#FFD5BD",
+        instorcolor: "#E3964A",
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *")
+      addVariant("child-hover", "& > *:hover")
+      addVariant("child-active", "& > *:active")
+    }
+  ],
 }
-
